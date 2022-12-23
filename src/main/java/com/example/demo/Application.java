aa689eca4f1bc69d6e95230211b8b6d58a34d4a5
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-12-23 11:41:41 -0600

    Jpa class student

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,11 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner (StudentRepository studentRepository){
+        return args -> {
+            Student maria = new Student("Maria", "Jones","maria.jones@amigoscode.com",15);
+            studentRepository.save(maria);
+        };
+    }
 }
